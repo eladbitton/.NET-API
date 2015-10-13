fa373353ea3774ce7bf3631116c9f1f33140a145
Copyleaks <Yehonatan@Copyleaks.com>
2015-10-13 16:17:11 +0300

    Added configuration files.

---
+++ SampleCode/CommandLineOptions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+namespace Copyleaks.SDK.SampleCode
+{
+	public class CommandLineOptions
+	{
+		[Option('u', "username", Required = true, HelpText = "Copyleaks account username")]
+		public string Username { get; set; }
+
+		[Option('k', "key", Required = true, HelpText = "Copyleaks account API key")]
+		public string ApiKey { get; set; }
+
+		[Option("url", Required = false, HelpText = "URL for scanning")]
+		public string URL { get; set; }
+
+		[Option("local-document", Required = false, HelpText = "Local document file to upload. Allowed formats (txt, rtf, doc, docx, pdf, ...).")]
+		public string LocalFile { get; set; }
+
+		[ParserState]
+		public IParserState LastParserState { get; set; }
+
+		[HelpOption]
+		public string GetUsage()
+		{
+			return HelpText.AutoBuild(this,
+			  (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+		}
+	}
+}
